a8ccdfaa3e65bdf7ad76cbad42902767ecfe2a2e
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-09-11 23:23:54 +0530

    Update App.js: add ScrollToTop component for smooth scrolling and enhance layout transitions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { BookingProvider } from './contexts/BookingContext';
 import { NotificationProvider } from './contexts/NotificationContext';
